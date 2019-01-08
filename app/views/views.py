28d2bb3365ac8667421540313e61743b86a79924
kanitamago <kanitamago5503@gmail.com>
2019-01-08 17:47:59 +0900

    google-get-photo-app

---
+++ app/views/views.py
@@ -0,0 +1,27 @@
+from app import app
+from app.scripts import get_photo
+from flask import render_template, request, url_for, redirect
+from google_images_download import google_images_download
+import os
+#亮介の足跡
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+@app.route("/result", methods=["GET", "POST"])
+def result():
+    if request.method == "POST":
+        try:
+            search_word = request.form["search-word"]
+            get_pages = request.form["get-pages"]
+            get_photo.options["keywords"] = search_word
+            get_photo.options["limit"] = int(get_pages)
+            #desktop_path = os.getenv("HOMEDRIVE") + os.getenv("HOMEPATH") + "\\Desktop"
+            desktop_path = os.path.expanduser("~") + "/Desktop"
+            get_photo.options["output_directory"] = desktop_path
+            get_photo.google_obj.download(get_photo.options)
+            return render_template("result.html", search_word=search_word, get_pages=get_pages, error=None)
+        except:
+            return render_template("result.html", search_word=None, get_pages=None, error="エラーが発生しました")
+    return redirect(url_for('index'))
