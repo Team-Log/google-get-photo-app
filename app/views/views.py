fc0eae76908563875555d550487b63a3baf436e1
kanitamago <kanitamago5503@gmail.com>
2019-01-08 18:57:26 +0900

    INDENT

--- app/views/views.py
+++ app/views/views.py
@@ -5,9 +5,6 @@ from google_images_download import google_images_download
 import os
 #亮介の足跡
 
-def show_image(path):
-
-
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
